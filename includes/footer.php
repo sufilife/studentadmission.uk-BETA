e9eeed689fa61f6cda401d644f8a558f7cba5492
Mustafa Sufian Soury <the.life.of.a.sufi@gmail.com>
2026-02-18 21:14:45 +0600

    Revamp footer structure and content
    Updated footer content with contact information and social media links.

--- includes/footer.php
+++ includes/footer.php
@@ -1,29 +1,26 @@
-<!-- Footer Start -->
-<div class="container-fluid bg-dark text-light footer py-5 wow fadeIn" data-wow-delay="0.1s">
-  <div class="container text-center py-5">
-    <a href="index.html">
-      <h1 class="display-4 mb-3 text-white text-uppercase">STUDENT ADMISSION LONDON</h1>
-    </a>
-    <div class="d-flex justify-content-center mb-4">
-      <a class="btn btn-lg-square btn-outline-primary border-2 m-1" href=""><i class="fab fa-x-twitter"></i></a>
-      <a class="btn btn-lg-square btn-outline-primary border-2 m-1" href=""><i class="fab fa-facebook-f"></i></a>
-      <a class="btn btn-lg-square btn-outline-primary border-2 m-1" href=""><i class="fab fa-youtube"></i></a>
-      <a class="btn btn-lg-square btn-outline-primary border-2 m-1" href=""><i class="fab fa-linkedin-in"></i></a>
-    </div>
-    <p>&copy; <a class="border-bottom" href="#">STUDENT ADMISSION LONDON</a>, All Right Reserved.</p>
-    <p class="mb-0">Designed By <a class="border-bottom" href="https://htmlcodex.com">Mustafa Sufian Soury</a></p>
-  </div>
-</div>
-<!-- Footer End -->
-<!-- Back to Top -->
-<a href="#" class="btn btn-outline-primary border-2 btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
-<!-- JavaScript Libraries -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="lib/wow/wow.min.js"></script>
-<script src="lib/easing/easing.min.js"></script>
-<script src="lib/waypoints/waypoints.min.js"></script>
-<script src="lib/owlcarousel/owl.carousel.min.js"></script>
-<script src="lib/lightbox/js/lightbox.min.js"></script>
-<!-- Template Javascript -->
-<script src="js/main.js"></script>
+    <!-- FOOTER -->
+    <footer>
+      <div class="footer-content">
+        <div>
+          <div style="font-weight:700">STUDENT ADMISSION LONDON</div>
+          <div>Office: 167 Green St, London E7 8JE, UK</div>
+          <div>WhatsApp: +447446969466</div>
+          <div>Email: admin@studentadmission.uk</div>
+        </div>
+        <div class="social">
+          <span>Follow Us on Social Media</span>
+         <a href="#" style="color: #000000; font-size: 24px;">
+    <i class="fa-brands fa-x-twitter"></i>
+</a>
+
+          <a href="#"><i class="fa-brands fa-facebook"></i> facebook </a>
+          <a href="#"><i class="fa-brands fa-youtube"></i> youtube </a>
+          <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        </div>
+        <div style="text-align:center;width:100%;margin-top:12px">
+          <div>© <a href="https://studentadmission.uk/" target="_blank" style="border-bottom:1px solid #000001;text-decoration:none">STUDENT ADMISSION LONDON</a>, All Rights Reserved.</div>
+          <div>Designed By <a href="https://www.facebook.com/mssoury" target="_blank" style="border-bottom:1px solid #000001;text-decoration:none">Mustafa Sufian Soury</a></div>
+        </div>
+      </div>
+    </footer>
