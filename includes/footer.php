49808f9c3c356f0623f0b9310da22e704e65a8f1
Mustafa Sufian Soury <the.life.of.a.sufi@gmail.com>
2026-03-08 02:20:17 +0600

    Refactor footer structure and update social links

--- includes/footer.php
+++ includes/footer.php
@@ -1,26 +1,24 @@
-    <!-- FOOTER -->
-    <footer>
-      <div class="footer-content">
-        <div>
-          <div style="font-weight:700">STUDENT ADMISSION LONDON</div>
-          <div>Office: 167 Green St, London E7 8JE, UK</div>
-          <div>WhatsApp: +447446969466</div>
-          <div>Email: admin@studentadmission.uk</div>
-        </div>
-        <div class="social">
-          <span>Follow Us on Social Media</span>
-         <a href="#" style="color: #000000; font-size: 24px;">
-    <i class="fa-brands fa-x-twitter"></i>
-</a>
+<footer>
+  <div class="footer-content">
+    <div>
+      <div style="font-weight:700">STUDENT ADMISSION LONDON</div>
+      <div>Office: 167 Green St, London E7 8JE, UK</div>
+      <div>WhatsApp: +447446969466</div>
+      <div>Email: admin@studentadmission.uk</div>
+    </div>
+    
+    <div class="social">
+      <span style="font-size: 13px;">Follow Us:</span>
+      <a href="#" aria-label="Twitter"><i class="fa-brands fa-x-twitter"></i></a>
+      <a href="#" aria-label="Facebook"><i class="fa-brands fa-facebook"></i></a>
+      <a href="#" aria-label="YouTube"><i class="fa-brands fa-youtube"></i></a>
+      <a href="#" aria-label="LinkedIn"><i class="fa-brands fa-linkedin"></i></a>
+      <a href="#" aria-label="Instagram"><i class="fa-brands fa-instagram"></i></a>
+    </div>
 
-          <a href="#"><i class="fa-brands fa-facebook"></i> facebook </a>
-          <a href="#"><i class="fa-brands fa-youtube"></i> youtube </a>
-          <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-          <a href="#"><i class="fa-brands fa-instagram"></i></a>
-        </div>
-        <div style="text-align:center;width:100%;margin-top:12px">
-          <div>© <a href="https://studentadmission.uk/" target="_blank" style="border-bottom:1px solid #000001;text-decoration:none">STUDENT ADMISSION LONDON</a>, All Rights Reserved.</div>
-          <div>Designed By <a href="https://www.facebook.com/mssoury" target="_blank" style="border-bottom:1px solid #000001;text-decoration:none">Mustafa Sufian Soury</a></div>
-        </div>
-      </div>
-    </footer>
+    <div style="text-align:center;width:100%;margin-top:12px; font-size: 12px; opacity: 0.8;">
+      <div>© <a href="https://studentadmission.uk/" target="_blank" style="border-bottom:1px solid #000001;text-decoration:none; color:inherit;">STUDENT ADMISSION LONDON</a>, All Rights Reserved.</div>
+      <div>Designed By <a href="https://www.facebook.com/mssoury" target="_blank" style="border-bottom:1px solid #000001;text-decoration:none; color:inherit;">Mustafa Sufian Soury</a></div>
+    </div>
+  </div>
+</footer>
